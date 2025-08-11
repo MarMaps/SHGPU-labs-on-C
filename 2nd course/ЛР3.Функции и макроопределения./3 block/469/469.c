7c857458d8915c0558df855acfcc9537f8323143
MarMaps <mgolubeva468@gmail.com>
2025-08-11 10:56:35 +0500

    add 3 block

---
+++ 2nd course/ЛР3.Функции и макроопределения./3 block/469/469.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VIR(s,n) \
+    if (n <= strlen(s)) exit(1); \
+    char buff_s[100] = ""; \
+    strcpy(buff_s, s); \
+    char *words[10]; \
+    char *pb; \
+    pb = strtok(buff_s, " "); \
+    int i = 0; \
+    while (pb != NULL) { \
+        words[i] = pb; \
+        pb = strtok(NULL, " "); \
+        i++; \
+    } \
+    int count_word = i; \
+    if (count_word == 1) { \
+       s[0] = '\0';  \
+       int space = n - strlen(words[0]); \
+       for (int i = 0; i < space; i++) { \
+           strcat(s, " "); \
+       } \
+		strcat(s, words[0]); \
+    } \
+    int prom = count_word - 1; \
+    int count_chars = 0; \
+    for (int i = 0; i < count_word; i++) { \
+        count_chars += strlen(words[i]); \
+        printf("count_chars = %d\n", count_chars); \
+    } \
+    int count_spaces = n - count_chars; \
+    int prom_space = count_spaces / prom; \
+    int lost_space = count_spaces % prom; \
+    s[0] = '\0'; \
+    for (int i = 0; i < count_word; i++) { \
+        strcat(s, words[i]); \
+        if (i < count_word-1) { \
+            for (int j = 0; j < prom_space + (lost_space > 0); j++) { \
+                strcat(s, " "); \
+            } \
+            if (lost_space > 0) { \
+				lost_space--; \
+			} \
+        } \
+    } \
+
+
+int main(int argc, char **argv, char **env)
+{
+    char s[100] = "";
+    printf("строка: ");
+    fgets(s, 100, stdin);
+    char *p = strchr(s, '\n');
+    *p = '\0';
+
+    int n;
+    printf("новая длина: ");
+    scanf("%d", &n);
+
+    VIR(s, n);
+    
+    printf("s = %s\n", s);
+    printf("len = %zu\n", strlen(s));
+    return 0;
+}
